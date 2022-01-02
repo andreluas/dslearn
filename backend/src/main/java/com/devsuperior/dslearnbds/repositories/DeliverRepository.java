120fde9552ccc0eb18ffc43baa86afe7eef9d1a4
Andre Almeida <andreluas.k@gmail.com>
2022-01-02 17:07:36 -0200

    repositories fix annotation

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
@@ -3,7 +3,9 @@ package com.devsuperior.dslearnbds.repositories;
 import com.devsuperior.dslearnbds.entities.Deliver;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeliverRepository extends JpaRepository<Deliver, Long> {
     
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
@@ -4,7 +4,9 @@ import com.devsuperior.dslearnbds.entities.Enrollment;
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentPK> {
     
 }

f7cd57932abd8ee3c79e1e6188b68829806d8eb9
Andre Almeida <andreluas.k@gmail.com>
2022-01-02 15:55:00 -0200

    clone nelio

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -14,102 +16,111 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_lesson")
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Lesson implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private Integer position;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Integer position;
+	
+	@ManyToOne
+	@JoinColumn(name = "section_id")
+	private Section section;
+	
+	@OneToMany(mappedBy = "lesson")
+	private List<Deliver> deliveries = new ArrayList<>();
+	
+	@ManyToMany
+	@JoinTable(name = "tb_lessons_done",
+		joinColumns = @JoinColumn(name = "lesson_id"),
+		inverseJoinColumns = {
+				@JoinColumn(name = "user_id"),
+				@JoinColumn(name = "offer_id")
+		}
+	)
+	private Set<Enrollment> enrollmentsDone = new HashSet<>();
+	
+	public Lesson() {
+	}
 
-    @ManyToOne
-    @JoinColumn(name = "section_id")
-    private Section section;
+	public Lesson(Long id, String title, Integer position, Section section) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.position = position;
+		this.section = section;
+	}
 
-    @ManyToMany
-    @JoinTable(name = "tb_lessons_done", joinColumns = @JoinColumn(name = "lesson_id"), inverseJoinColumns = {
-            @JoinColumn(name = "user_id"), @JoinColumn(name = "offer_id") })
-    private Set<Enrollment> enrollmentsDone = new HashSet<>();
+	public Long getId() {
+		return id;
+	}
 
-    public Lesson() {
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Lesson(Long id, String title, Integer position, Section section) {
-        this.id = id;
-        this.title = title;
-        this.position = position;
-        this.section = section;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Integer getPosition() {
+		return position;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public Section getSection() {
+		return section;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setSection(Section section) {
+		this.section = section;
+	}
 
-    public Integer getPosition() {
-        return position;
-    }
+	public Set<Enrollment> getEnrollmentsDone() {
+		return enrollmentsDone;
+	}
 
-    public void setPosition(Integer position) {
-        this.position = position;
-    }
+	public List<Deliver> getDeliveries() {
+		return deliveries;
+	}
 
-    public Section getSection() {
-        return section;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    public void setSection(Section section) {
-        this.section = section;
-    }
-
-    public Set<Enrollment> getEnrollmentDone() {
-        return enrollmentsDone;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Lesson other = (Lesson) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lesson other = (Lesson) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -24,31 +24,33 @@ public class Offer implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edition;
-
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant startMoment;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant endMoment;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
-
+	
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
-
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();	
+	
 	public Offer() {
 	}
 
-	public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course,
-			List<Resource> resources) {
+	public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
+		super();
 		this.id = id;
 		this.edition = edition;
 		this.startMoment = startMoment;
 		this.endMoment = endMoment;
 		this.course = course;
-		this.resources = resources;
 	}
 
 	public Long getId() {
@@ -90,9 +92,13 @@ public class Offer implements Serializable {
 	public void setCourse(Course course) {
 		this.course = course;
 	}
-
+	
 	public List<Resource> getResources() {
 		return resources;
+	}
+
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override

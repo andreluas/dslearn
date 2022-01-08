c382c2c957341e1accf497088363126a212f2fc2
Andre Almeida <andreluas.k@gmail.com>
2022-01-08 18:40:57 -0300

    refreshToken

--- backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -46,6 +47,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtTokenEnchancer tokenEnchancer;
 
+    @Autowired
+    private UserDetailsService userDetailsService; 
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -53,8 +57,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient(cliendId).secret(passwordEncoder.encode(cliendSecret)).scopes("read", "write")
-                .authorizedGrantTypes("password").accessTokenValiditySeconds(jwtDuration);
+        clients.inMemory()
+        .withClient(cliendId)
+        .secret(passwordEncoder.encode(cliendSecret))
+        .scopes("read", "write")        
+        .authorizedGrantTypes("password", "refresh_token")
+        .accessTokenValiditySeconds(jwtDuration)
+        .refreshTokenValiditySeconds(jwtDuration);
     }
 
     @Override
@@ -63,8 +72,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         TokenEnhancerChain chain = new TokenEnhancerChain();
         chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnchancer));
 
-        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore)
-                .accessTokenConverter(accessTokenConverter).tokenEnhancer(chain);
+        endpoints.authenticationManager(authenticationManager)
+        .tokenStore(tokenStore)
+        .accessTokenConverter(accessTokenConverter)
+        .tokenEnhancer(chain)
+        .userDetailsService(userDetailsService);
     }
 
 }
